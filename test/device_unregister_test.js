13d94dcf84e1bef2fed15015030985038d0b4f44
Simon <sjd3002@gmail.com>
2021-06-14 14:31:33 -0600

    adds device unregister test

--- test/device_unregister_test.js
+++ test/device_unregister_test.js
@@ -6,13 +6,40 @@ const server = require('../app');
 const app = require('../app');
 
 chai.use(chaiHttp);
+const faker = require('faker');
 
-describe('/GET device/unregister', () => {
-  it('it should GET the information', (done) => {
-    chai.request(server)
-      .get('/device/unregister')
+const temp_mac = faker.internet.mac();
+
+//register a temp device
+chai.request(app)
+      .post('/device/register')
+      .send({ deviceId: temp_mac, friendlyName: faker.internet.userName() })
+      .end((err, res) => {
+      })
+
+//test case: attempt to remove the device
+describe('/POST device/unregister', () => {
+  it('it should use POST to remove the information', (done) => {
+    chai.request(app)
+      .post('/device/unregister')
+      .send({ deviceId: temp_mac })
       .end((err, res) => {
         assert.equal(res.statusCode, 200);
+        done();
+      })
+  });
+});
+
+//test case: verify the device is gone
+//should return 404 indicating the db entry is not found
+describe('/GET /device/info verify the entry is gone', () => {
+  it('it should use GET to verify the device is gone', (done) => {
+    chai.request(server)
+      .get('/device/info')
+      .end((err, res) => {
+        assert.equal(res.statusCode, 404);
+        done();
       });
   });
 });
+
