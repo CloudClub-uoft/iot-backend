ab53f7ece06b9641f40fbf9a389f5267d1f8acd7
Bryce Lepasana <lepasanabryce@gmail.com>
2021-06-16 21:53:58 -0400

    Create device info GET request in device/info route

--- routes/device/GET-info.js
+++ routes/device/GET-info.js
@@ -1,9 +1,21 @@
+const Device = require('../../models/device');
 module.exports = (app, db) => {
   app.get('/device/info', (req, res, next) => {
     // TODO
     // INPUT: API key
     // the device information is provided
-    // OUTPUT: firendly name
-    res.sendStatus(404);
+    // OUTPUT: friendly name
+    const apiKey = req.body.apiKey;
+    Device.findOne({apiKey: apiKey}, 'friendlyName', function (err, foundDevice) {
+      if (err) {
+        return res.status(400);
+      }
+      else if (foundDevice == null) {
+        return res.status(404);
+      }
+      else {
+        return res.status(200).json({ friendlyName: foundDevice.friendlyName });
+      }
+    });
   });
 }
