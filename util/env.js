634f83707b8317755da311cd4308a60658062eec
Matthew Mao <matty8100@gmail.com>
2021-06-20 16:47:05 -0700

    handle no env file error

--- util/env.js
+++ util/env.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 exports.configure = () => {
   const result = dotenv.config();
   if (result.error) {
+    if (result.error.code === 'ENOENT') return;
     throw result.error;
   }
   return 0;
