1432a03cc8534c60889cd4d12805300eea210886
Matthew Mao <matty8100@gmail.com>
2021-06-19 18:36:05 -0700

    take MAC address query from URL instead of body

--- routes/device/GET-info.js
+++ routes/device/GET-info.js
@@ -1,21 +1,15 @@
 const Device = require('../../models/device');
-module.exports = (app, db) => {
-  app.get('/device/info', (req, res, next) => {
-    // TODO
-    // INPUT: API key
-    // the device information is provided
-    // OUTPUT: friendly name
-    const apiKey = req.body.apiKey;
-    Device.findOne({apiKey: apiKey}, 'friendlyName', function (err, foundDevice) {
-      if (err) {
-        return res.status(400);
-      }
-      else if (foundDevice == null) {
-        return res.status(404);
-      }
-      else {
-        return res.status(200).json({ friendlyName: foundDevice.friendlyName });
-      }
+
+module.exports = (app) => {
+  app.get('/device/info', (req, res) => {
+    if (!req.query.mac) return res.status(400);
+    // Take MAC address with no symbols as query parameter and reformat with colons
+    const mac = req.query.mac.replace(/(..)/g, '$1:').slice(0, -1);
+    Device.findOne({ deviceId: mac }, 'friendlyName', (err, foundDevice) => {
+      if (err) return res.status(400);
+      if (foundDevice == null) return res.status(404);
+
+      return res.status(200).json({ friendlyName: foundDevice.friendlyName });
     });
   });
-}
+};
