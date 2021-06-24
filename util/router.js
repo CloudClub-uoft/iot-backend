1b00877dc3f8168433a5f542279d0fa990118be3
Matthew Mao <matty8100@gmail.com>
2021-06-24 12:51:20 -0700

    rename and move files for clarity

--- util/router.js
+++ util/router.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const routePath = path.resolve(__dirname);
+const routePath = path.resolve('./routes');
 
 exports.boot = (app) => {
   (function traverse(dir) {
@@ -9,11 +9,10 @@ exports.boot = (app) => {
       const dirPath = path.join(dir, file);
       const isDirectory = fs.lstatSync(dirPath).isDirectory();
       if (isDirectory) traverse(dirPath);
-      if (file !== 'routing.js' && !isDirectory) {
-      // routes/filename, slicing off '.js'
+      if (!isDirectory) {
+        // routes/filename, slicing off '.js'
         const cleanPath = `${dir}/${file.substr(0, file.indexOf('.'))}`;
         const route = path.resolve(cleanPath);
-        // load the route
         // eslint-disable-next-line import/no-dynamic-require, global-require
         require(route)(app);
       }
