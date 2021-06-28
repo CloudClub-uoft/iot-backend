3802a8909940b30d9935a10f5987dc34a79da0bd
Bryce Lepasana <lepasanabryce@gmail.com>
2021-06-28 15:44:41 -0400

    Change findOneAndUpdate() to create()

--- routes/data/POST-data.js
+++ routes/data/POST-data.js
@@ -4,15 +4,14 @@ const Device = require('../../models/device');
 module.exports = (app) => {
     app.post('/data/new', (req, res) => {
         const { apiKey, deviceId, temperature, location } = req.body;
-        const newData = { temperature: temperature, location: location, timestamp: Date.now() };
-        const options = { upsert: true };
+        const newData = { deviceId:deviceId, temperature: temperature, location: location, timestamp: Date.now() };
         Device.findOne({ apiKey: apiKey }, (err, foundDevice) => {
             if (err) return res.status(400).json({ error: 'Bad Request' });
             if (foundDevice == null) return res.status(401).json({ error: 'Unauthorized' });
-            Data.findOneAndUpdate({deviceId}, newData, options, (err, Data) =>{
+            Data.create( newData, (err, Data) =>{
                 if (err) return res.status(400).json({ error: 'Bad Request' });
+                return res.sendStatus(201);
             });
-            return res.sendStatus(201);
         });
     });
 };
