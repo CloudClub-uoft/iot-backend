6346d0f46e4593e42d19027c6e6f95b69605699c
Matthew Mao <matty8100@gmail.com>
2021-07-05 01:54:26 -0700

    implement TLS certificate request route

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ require('./util/env').configure(); // configure the environment variables
 require('./util/mongodb'); // configure mongoose db connection
 require('./util/aedes'); // configure MQTT broker
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 // Express config
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Dynamic route loading
 require('./util/router').boot(app);
---
+++ routes/auth/GET-cert.js
@@ -0,0 +1,64 @@
+const jwt = require('jsonwebtoken');
+const forge = require('node-forge');
+const fs = require('fs');
+
+module.exports = (app) => {
+  app.get('/auth/cert', (req, res) => {
+    const { token } = req.cookies || {};
+    if (!token) return res.status(401).json({ error: 'Not logged in.' });
+
+    let payload;
+    try {
+      payload = jwt.verify(token, process.env.JWT_KEY);
+    } catch (error) {
+      if (error instanceof jwt.JsonWebTokenError) return res.status(401).json({ Error: 'Unauthorized 401' });
+      return res.status(400).json({ Error: 'Bad Request 400' });
+    }
+
+    const caCert = forge.pki.certificateFromPem(fs.readFileSync(process.env.MQTT_SERVER_CA_PATH));
+    const caKey = forge.pki.privateKeyFromPem(fs.readFileSync(process.env.MQTT_SERVER_KEY_PATH));
+
+    const clientKeys = forge.pki.rsa.generateKeyPair(4096);
+    const clientCert = forge.pki.createCertificate();
+    clientCert.publicKey = clientKeys.publicKey;
+    clientCert.serialNumber = '01';
+    clientCert.validity.notBefore = new Date();
+    clientCert.validity.notAfter = new Date();
+    clientCert.validity.notAfter.setDate(clientCert.validity.notBefore.getDate() + 365);
+
+    const attributes = [{
+      name: 'commonName',
+      value: 'IoT Device',
+    }, {
+      name: 'countryName',
+      value: 'CA',
+    }, {
+      name: 'localityName',
+      value: 'Toronto',
+    }, {
+      name: 'stateOrProvinceName',
+      value: 'ON',
+    }, {
+      name: 'organizationName',
+      value: 'CloudClub',
+    }, {
+      name: 'organizationalUnitName',
+      value: 'IoT-Project',
+    }, {
+      name: 'emailAddress',
+      value: payload.email,
+    }];
+
+    clientCert.setSubject(attributes);
+    clientCert.setIssuer(caCert.subject.attributes);
+
+    clientCert.sign(caKey, forge.md.sha256.create());
+
+    const pem = {
+      privateKey: forge.pki.privateKeyToPem(clientKeys.privateKey),
+      certificate: forge.pki.certificateToPem(clientCert),
+    };
+
+    return res.status(200).json(pem);
+  });
+};
