48cc37d16b3e7b1adc7c3670ae06c4898ab7a392
Bryce Lepasana <lepasanabryce@gmail.com>
2021-07-06 19:20:41 -0400

    Create getData route request

--- routes/data/GET-data.js
+++ routes/data/GET-data.js
@@ -1,5 +1,15 @@
+const Data = require('../../models/data');
+
 module.exports = (app) => {
     app.get('/device/getData', (req, res) => {
-        res.sendStatus(404);
+        if (!req.query.mac) return res.status(400).json({ error: 'No MAC address provided.' });
+        if (!req.query.points) return res.status(400).json({ error: 'Please specify number of data points.' });
+        mac = req.query.mac.replace(/(..)/g, '$1:').slice(0, -1);
+        points = Number(req.query.points);
+        Data.find({ deviceId: mac }).sort({timestamp: -1}).limit(points).exec((err, Data) => {
+            if (err) return res.status(400).json({ error: 'Bad Request' });
+            if (Data == null) return res.status(404).json({ error: 'Not found.' });
+            return res.status(200).json({ Data });
+        })
     });
 };
