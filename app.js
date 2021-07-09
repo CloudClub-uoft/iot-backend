28939f48195f6093a5f417b9621d2ab20793bf99
Matthew Mao <matty8100@gmail.com>
2021-07-08 21:19:26 -0700

    add cors middleware to express

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ require('./util/env').configure(); // configure the environment variables
 require('./util/mongodb'); // configure mongoose db connection
 require('./util/aedes'); // configure MQTT broker
 const express = require('express');
+const cors = require('cors');
 
 // Express config
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// TODO add production check for TLS or not
+app.use(cors({ origin: `http://localhost:${process.env.WEBAPP_PORT}` }));
 
 // Dynamic route loading
 require('./util/router').boot(app);
