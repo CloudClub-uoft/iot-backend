9c9dc72f2d60fa7253faec0d101e9d43c5348592
Bryce Lepasana <lepasanabryce@gmail.com>
2021-07-09 20:57:00 -0400

    fix code stlye via eslint

--- routes/data/POST-new.js
+++ routes/data/POST-new.js
@@ -2,16 +2,20 @@ const Data = require('../../models/data');
 const Device = require('../../models/device');
 
 module.exports = (app) => {
-    app.post('/data/new', (req, res) => {
-        const { apiKey, deviceId, temperature, location } = req.body;
-        const newData = { deviceId:deviceId, temperature: temperature, location: location, timestamp: Date.now() };
-        Device.findOne({ apiKey: apiKey }, (err, foundDevice) => {
-            if (err) return res.status(400).json({ error: 'Bad Request' });
-            if (foundDevice == null) return res.status(401).json({ error: 'Unauthorized' });
-            Data.create( newData, (err, Data) =>{
-                if (err) return res.status(400).json({ error: 'Bad Request' });
-                return res.sendStatus(201);
-            });
-        });
+  app.post('/data/new', (req, res) => {
+    const {
+      apiKey, deviceId, temperature, location,
+    } = req.body;
+    const newData = {
+      deviceId, temperature, location, timestamp: Date.now(),
+    };
+    Device.findOne({ apiKey }, (err, foundDevice) => {
+      if (err) return res.status(400).json({ error: 'Bad Request' });
+      if (foundDevice == null) return res.status(401).json({ error: 'Unauthorized' });
+      Data.create(newData, (dataError, _) => {
+        if (dataError) return res.status(400).json({ error: 'Bad Request' });
+        return res.sendStatus(201);
+      });
     });
+  });
 };
