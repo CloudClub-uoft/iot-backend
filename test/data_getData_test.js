0679d8d92ffbf5a94c4e6a7da1afb7b56acd7baf
Harsimrat Singh <singh21.h@gmail.com>
2021-07-15 22:58:39 -0400

    Fix failing tests

--- test/data_getData_test.js
+++ test/data_getData_test.js
@@ -7,59 +7,64 @@ const app = require('../app');
 const Device = require('../models/device');
 const Data = require('../models/data');
 
-const { expect } = chai;
+const {expect} = chai;
 chai.use(chaiHttp);
 
 describe('/GET data/getData', () => {
-  const tempMac = faker.internet.mac();
-  const tempName = faker.internet.userName();
-  const tempTemperature = faker.datatype.number();
-  const tempLocation = {
-    type: 'Point',
-    coordinates: [Number(faker.address.latitude()), Number(faker.address.longitude())],
-  };
-  const { uuid, apiKey } = uuidApiKey.create();
 
-  before(() => {
-    // Register a temp device and Post temporary data
-    new Device({
-      deviceId: tempMac,
-      friendlyName: tempName,
-      uuid,
-      apiKey,
-    }).save();
-    new Data({
-      apiKey,
-      deviceId: tempMac,
-      temperature: tempTemperature,
-      location: tempLocation,
-    }).save();
-  });
+    const tempMac = faker.internet.mac();
+    const tempName = faker.internet.userName();
+    const tempTemperature = faker.datatype.number();
+    const tempLocation = {
+        type: 'Point',
+        coordinates: [Number(faker.address.latitude()), Number(faker.address.longitude())],
+    };
+    const {uuid, apiKey} = uuidApiKey.create();
 
-  it('it should GET the data', (done) => {
+    let putData =  function() {
 
-    //remove MAC Address colons
-    const changedMac = tempMac.replace(/:/g, '');
+        // Register a temp device and Post temporary data
+        new Data({
+            apiKey,
+            deviceId: tempMac,
+            temperature: tempTemperature,
+            location: tempLocation,
+        }).save().then(() => {
+            //blank
+        });
 
-    //Create New URL
-    const requestURL = `/data/getData?mac=${changedMac}&points=1`;
+    }
 
-    console.log(requestURL);
+    before(() => {
 
-    chai.request(app)
-      .get(requestURL)
-      .end((_, res) => {
-        expect(res.statusCode).to.equal(200);
-        const parseData = JSON.parse(JSON.stringify(res.body.doc));
-        expect(parseData[0].location.coordinates).to.deep.equal(tempLocation.coordinates);
-        expect(Number(parseData[0].temperature)).to.equal(Number(tempTemperature));
-        done();
-      });
-  });
+        putData();
 
-  after(() => {
-    // Delete temp device and temp data
-    Data.deleteOne({ deviceId: tempMac });
-    Device.deleteOne({ deviceId: tempMac });
-  });
+    });
+
+    it('it should GET the data', (done) => {
+
+        //remove MAC Address colons
+        const changedMac = tempMac.replace(/:/g, '');
+
+        //Create New URL
+        const requestURL = `/data/getData?mac=${changedMac}&points=1`;
+
+        console.log(requestURL);
+
+        chai.request(app)
+            .get(requestURL)
+            .end((_, res) => {
+                expect(res.statusCode).to.equal(200);
+                const parseData = JSON.parse(JSON.stringify(res.body.doc));
+                expect(parseData[0].location.coordinates).to.deep.equal(tempLocation.coordinates);
+                expect(Number(parseData[0].temperature)).to.equal(Number(tempTemperature));
+                done();
+            });
+    });
+
+    after(() => {
+        // Delete temp device and temp data
+        Data.deleteOne({deviceId: tempMac});
+        Device.deleteOne({deviceId: tempMac});
+    });
 });
