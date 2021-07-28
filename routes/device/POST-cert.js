c6fb0c65902be8101567dd030d1af9c48edbb5c7
Matthew Mao <matty8100@gmail.com>
2021-07-28 16:12:35 -0700

    change device/cert to POST and add test

--- routes/device/POST-cert.js
+++ routes/device/POST-cert.js
@@ -2,7 +2,7 @@ const login = require('../../util/login');
 const genCert = require('../../util/genCert');
 
 module.exports = (app) => {
-  app.get('/device/cert', (req, res) => {
+  app.post('/device/cert', (req, res) => {
     login(req, res, (email) => {
       const der = genCert(email, 'der');
       return res.status(200).json(der);
