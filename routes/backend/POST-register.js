c8ab64d1e4f28970e6106d76b6878cc9e02571ca
Matthew Mao <matty8100@gmail.com>
2021-07-30 13:21:45 -0700

    use middleware for JWT verification

--- routes/backend/POST-register.js
+++ routes/backend/POST-register.js
@@ -1,18 +1,10 @@
-const jwt = require('jsonwebtoken');
 const uuidApiKey = require('uuid-apikey');
 
+const jwtVerify = require('../../middleware/jwtVerify');
 const Device = require('../../models/device');
 
 module.exports = (app) => {
-  app.post('/device/register', (req, res) => {
-    const { token } = req.cookies || {};
-    try {
-      jwt.verify(token, process.env.JWT_KEY);
-    } catch (error) {
-      if (error instanceof jwt.JsonWebTokenError) return res.status(401).json({ Error: 'Unauthorized 401' });
-      return res.status(400).json({ Error: 'Bad Request 400' });
-    }
-
+  app.post('/device/register', jwtVerify, (req, res) => {
     const { deviceId, friendlyName } = req.body;
     if (!deviceId || !deviceId.match(/([0-9A-Fa-f]{2}){6}/) || !friendlyName) return res.status(400).json({ error: 'Bad Request' });
     const { uuid, apiKey } = uuidApiKey.create();
--- routes/backend/POST-unregister.js
+++ routes/backend/POST-unregister.js
@@ -1,17 +1,8 @@
-const jwt = require('jsonwebtoken');
-
+const jwtVerify = require('../../middleware/jwtVerify');
 const Device = require('../../models/device');
 
 module.exports = (app) => {
-  app.post('/device/unregister', (req, res) => {
-    const { token } = req.cookies || {};
-    try {
-      jwt.verify(token, process.env.JWT_KEY);
-    } catch (error) {
-      if (error instanceof jwt.JsonWebTokenError) return res.status(401).json({ Error: 'Unauthorized 401' });
-      return res.status(400).json({ Error: 'Bad Request 400' });
-    }
-
+  app.post('/device/unregister', jwtVerify, (req, res) => {
     const deviceId = req.body.deviceId.toLowerCase();
 
     Device.findOneAndDelete({ deviceId }, (err, foundDevice) => {
