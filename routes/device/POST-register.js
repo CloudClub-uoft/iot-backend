2de04ae272b0c0e49f63e3c0344228e402515704
Lepabryce <Lepasanabryce@gmail.com>
2021-08-30 17:14:38 -0400

    Register with email and verify with other requests

--- routes/device/POST-register.js
+++ routes/device/POST-register.js
@@ -1,14 +1,21 @@
+const e = require('express');
 const jwtVerify = require('../../middleware/jwtVerify');
 const Device = require('../../models/device');
 
 module.exports = (app) => {
   app.post('/device/register', jwtVerify, (req, res) => {
+    const userEmail = res.locals.jwtPayload.email;
     const { deviceId, friendlyName } = req.body;
     if (!deviceId || !deviceId.match(/([0-9A-Fa-f]{2}){6}/) || !friendlyName) return res.status(400).json({ error: 'Bad Request' });
-
-    new Device({
-      deviceId: deviceId.toLowerCase(),
-      friendlyName,
-    }).save().then(() => res.status(200).json({ 'Status:': 'Success' }));
+    Device.findOne({ deviceId }, (err, foundDevice) => {
+      if (err) return res.status(400).json({ error: 'Bad Request' });
+      if (foundDevice) return res.status(409).json({ error: 'Already Registered' });
+      new Device({
+        deviceId: deviceId.toLowerCase(),
+        friendlyName,
+        userEmail,
+      }).save();
+      return res.status(200).json({ 'Status:': 'Success' });
+    });
   });
 };
--- routes/device/POST-unregister.js
+++ routes/device/POST-unregister.js
@@ -4,11 +4,21 @@ const Device = require('../../models/device');
 module.exports = (app) => {
   app.post('/device/unregister', jwtVerify, (req, res) => {
     const deviceId = req.body.deviceId.toLowerCase();
-
-    Device.findOneAndDelete({ deviceId }, (err, foundDevice) => {
+    const userEmail = res.locals.jwtPayload.email;
+    let deviceOwner = true;
+    Device.findOne({ deviceId }, (err, foundDevice) => {
       if (err) return res.status(400).json({ error: 'Bad Request' });
-      if (foundDevice == null) return res.status(404).json({ error: 'No device found.' });
-      return res.status(200).json({ message: 'Device successfully unregistered.' });
+      if (foundDevice.userEmail !== userEmail) {
+        deviceOwner = false;
+        res.status(401).json({ Error: 'Unauthorized 401' });
+      }
     });
+    if (deviceOwner) {
+      Device.findOneAndDelete({ deviceId }, (err, foundDevice) => {
+        if (err) return res.status(400).json({ error: 'Bad Request' });
+        if (foundDevice == null) return res.status(404).json({ error: 'No device found.' });
+        return res.status(200).json({ message: 'Device successfully unregistered.' });
+      });
+    }
   });
 };
