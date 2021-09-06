68595cebba14d40c34f1dc70de19c52df46fb4de
Lepabryce <Lepasanabryce@gmail.com>
2021-09-06 14:39:39 -0400

    create device/userDevices test

---
+++ test/device_userDevices_test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const jwt = require('jsonwebtoken');
+const { deviceApp } = require('../app');
+const Device = require('../models/device');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('/GET device/userDevices', () => {
+  const tempMac = faker.internet.mac().replace(/:/g, '');
+  const tempName = faker.internet.userName();
+  const email = faker.internet.email();
+  let token;
+  before((done) => {
+    // Register a temp device
+    token = jwt.sign({ email }, process.env.JWT_KEY, {
+      algorithm: 'HS256',
+      expiresIn: 60,
+    });
+    new Device({
+      deviceId: tempMac,
+      friendlyName: tempName,
+      userEmail: email,
+    }).save().then(() => done());
+  });
+
+  it('it should GET the registered devices by the user', (done) => {
+    chai.request(deviceApp)
+      .get('/device/userDevices')
+      .set('Cookie', `token=${token}`)
+      .end((_, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.userDevices[0].friendlyName).to.equal(tempName);
+        done();
+      });
+  });
+  after((done) => { Device.deleteOne({ deviceId: tempMac }).then(() => done()); });
+});
